e24f72dc5a72b28670d0d7ca80d58bf7e730d938
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-06-17 23:11:35 -0400

    created cart and cart item components

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import Cart from './components/cart'
 
 /**
  * COMPONENT
@@ -19,6 +20,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/cart" component={Cart} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
