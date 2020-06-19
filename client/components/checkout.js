8ab4b2e3ee8637aabf338a9dcf001e0472ce6502
Vicky Rodriguez <vrod8763@gmail.com>
2020-06-18 21:42:31 -0400

    inverted checkout form

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -14,47 +14,51 @@ export class Checkout extends Component {
   render() {
     return (
       <div>
-        <Header as="h1">Order Summary</Header>
-        <div>
-          <Segment inverted>
-            <h3>order quantity: </h3>
-            <h3>order total: </h3>
-          </Segment>
-        </div>
-        <Header as="h1">Shipping and Billing Info</Header>
         <Segment inverted>
-          <Form inverted>
-            <Form.Group widths="equal">
-              <Form.Input fluid label="First name" placeholder="First name" />
-              <Form.Input fluid label="Last name" placeholder="Last name" />
-            </Form.Group>
-            <Form.Group widths="equal">
-              <Form.Input
-                fluid
-                label="SHIPPING ADDRESS"
-                placeholder="street address"
-              />
-              <Form.Input
-                fluid
-                label="CITY, STATE"
-                placeholder="i.e. New York, NY"
-              />
-            </Form.Group>
-            <Form.Group widths="equal">
-              <Form.Input fluid label="ZIP CODE" placeholder="Zip" />
-              <Form.Input fluid label="PHONE" placeholder="phone" />
-            </Form.Group>
-            <Form.Group widths="equal">
-              <Form.Input
-                fluid
-                label="CARD NUMBER"
-                placeholder="xxxx-xxxx-xxxx-xxxx"
-              />
-              <Form.Input fluid label="CVC" placeholder="CVC" />s
-            </Form.Group>
-            <Form.Checkbox label="I agree to the Terms and Conditions" />
-            <Button type="submit">Place Order</Button>
-          </Form>
+          <Header as="h1" inverted clor="grey">
+            Order Summary
+          </Header>
+          <div>
+            <Segment inverted>
+              <h3>order quantity: </h3>
+              <h3>order total: </h3>
+            </Segment>
+          </div>
+          <Header as="h1">Shipping and Billing Info</Header>
+          <Segment inverted>
+            <Form inverted>
+              <Form.Group widths="equal">
+                <Form.Input fluid label="First name" placeholder="First name" />
+                <Form.Input fluid label="Last name" placeholder="Last name" />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Input
+                  fluid
+                  label="SHIPPING ADDRESS"
+                  placeholder="street address"
+                />
+                <Form.Input
+                  fluid
+                  label="CITY, STATE"
+                  placeholder="i.e. New York, NY"
+                />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Input fluid label="ZIP CODE" placeholder="Zip" />
+                <Form.Input fluid label="PHONE" placeholder="phone" />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Input
+                  fluid
+                  label="CARD NUMBER"
+                  placeholder="xxxx-xxxx-xxxx-xxxx"
+                />
+                <Form.Input fluid label="CVC" placeholder="CVC" />s
+              </Form.Group>
+              <Form.Checkbox label="I agree to the Terms and Conditions" />
+              <Button type="submit">Place Order</Button>
+            </Form>
+          </Segment>
         </Segment>
       </div>
     )
