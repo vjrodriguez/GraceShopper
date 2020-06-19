87b3cfd3d4480a4e65bb89bc905de33a4cf27941
Vicky Rodriguez <vrod8763@gmail.com>
2020-06-18 21:45:25 -0400

    update OC page

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Segment} from 'semantic-ui-react'
 
 const OrderConfirmation = () => {
   return (
     <div>
-      <h2>Your order has been placed!</h2>
-      <h3>Thanks for getting polish'd with us</h3>
-      <button type="submit">Start New Order</button>
+      <Segment inverted>
+        <h2>Your order has been placed!</h2>
+        <h3>Thanks for getting polish'd with us</h3>
+        <button type="submit">Start New Order</button>
+      </Segment>
     </div>
   )
 }
