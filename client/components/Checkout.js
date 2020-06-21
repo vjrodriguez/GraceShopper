739f86baf3022a96bb3e7d804831917db91e21d7
Vicky Rodriguez <vrod8763@gmail.com>
2020-06-21 17:19:42 -0400

    added Reacr Stripe package and stripe component

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -8,8 +8,6 @@ export class Checkout extends Component {
   // eslint-disable-next-line no-useless-constructor
   constructor() {
     super()
-    this.state = {}
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   // async handleSubmit() here
@@ -27,40 +25,8 @@ export class Checkout extends Component {
               <h3>order total: </h3>
             </Segment>
           </div>
-          <Header as="h1">Shipping and Billing Info</Header>
           <Segment inverted>
-            <Form inverted>
-              <Form.Group widths="equal">
-                <Form.Input fluid label="First name" placeholder="First name" />
-                <Form.Input fluid label="Last name" placeholder="Last name" />
-              </Form.Group>
-              <Form.Group widths="equal">
-                <Form.Input
-                  fluid
-                  label="SHIPPING ADDRESS"
-                  placeholder="street address"
-                />
-                <Form.Input
-                  fluid
-                  label="CITY, STATE"
-                  placeholder="i.e. New York, NY"
-                />
-              </Form.Group>
-              <Form.Group widths="equal">
-                <Form.Input fluid label="ZIP CODE" placeholder="Zip" />
-                <Form.Input fluid label="PHONE" placeholder="phone" />
-              </Form.Group>
-              <Form.Group widths="equal">
-                <Form.Input
-                  fluid
-                  label="CARD NUMBER"
-                  placeholder="xxxx-xxxx-xxxx-xxxx"
-                />
-                <Form.Input fluid label="CVC" placeholder="CVC" />
-              </Form.Group>
-              <Form.Checkbox label="I agree to the Terms and Conditions" />
-              <Button type="submit">Place Order</Button>
-            </Form>
+            <Button type="submit">Buy Now</Button>
           </Segment>
         </Segment>
       </div>
