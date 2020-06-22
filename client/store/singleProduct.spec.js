44a5becdbb0b9c6706ea212d41f45257c038b5d6
Kate Norton <katernorton@outlook.com>
2020-06-22 11:42:07 -0400

    backend tests for products created

--- client/store/singleProduct.spec.js
+++ client/store/singleProduct.spec.js
@@ -41,21 +41,12 @@ describe('thunk creators', () => {
         colorFamily,
         stock
       }
-      mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
-      await store.dispatch(me())
+      const id = 1
+      mockAxios.onGet(`/api/products/${id}`).replyOnce(200, fakeProduct)
+      await store.dispatch(getSingleProduct(id))
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GET_USER')
-      expect(actions[0].user).to.be.deep.equal(fakeUser)
-    })
-  })
-
-  describe('logout', () => {
-    it('logout: eventually dispatches the REMOVE_USER action', async () => {
-      mockAxios.onPost('/auth/logout').replyOnce(204)
-      await store.dispatch(logout())
-      const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('REMOVE_USER')
-      expect(history.location.pathname).to.be.equal('/login')
+      expect(actions[0].type).to.be.equal('SET_PRODUCT')
+      expect(actions[0].product).to.be.deep.equal(fakeProduct)
     })
   })
 })
