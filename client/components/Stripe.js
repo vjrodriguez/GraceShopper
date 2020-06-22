5bc676b310f1188ab73ef3735b5cfd4564a84ee7
Vicky Rodriguez <vrod8763@gmail.com>
2020-06-22 12:05:05 -0400

    no progress on stripe

--- client/components/Stripe.js
+++ client/components/Stripe.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import axios from 'axios'
-import {Elements} from '@stripe/react-stripe-js'
-import {loadStripe} from '@stripe/react-stripe-js'
+
+// const stripe = await loadStripe('pk_test_51GwdBRDjkfM6HbyF3VyKJSE0OtwnJy3gLsAquq13f4ndU5BV5vYEpHivB7gWESh1dVNd6uGHgmisuf0IaJxd3pjx000DQA3SaM')
