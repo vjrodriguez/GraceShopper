da49586e843067b486b0b017b5b20a563f995b98
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-06-23 11:16:08 -0400

    feat: created UI for admin dashboard

--- client/components/AdminUserView.js
+++ client/components/AdminUserView.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Grid, Button} from 'semantic-ui-react'
+import {Grid, Button, Radio} from 'semantic-ui-react'
 
 export default function AdminUserView(props) {
   return (
     <Grid.Row columns={5} verticalAlign="middle">
       <Grid.Column>FirstName</Grid.Column>
       <Grid.Column>LastName</Grid.Column>
-      <Grid.Column>email@email.com</Grid.Column>
+      <Grid.Column>email@email.com </Grid.Column>
       <Grid.Column>
         <Button>Orders</Button>
       </Grid.Column>
